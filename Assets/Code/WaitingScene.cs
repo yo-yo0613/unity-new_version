bd52b4a7a4fb38dfb6f4e68bf50a1bd522c17bfe
yo-yo0613 <chengyouli37@gmail.com>
2025-05-30 17:09:07 +0800

    commit

---
+++ Assets/Code/WaitingScene.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MidiJack;
+
+public class WaitingScene : MonoBehaviour
+{
+    void Update()
+    {
+        StartCoroutine(WaitAndLoadScene());
+    }
+}
+
+IEnumerator WaitAndLoadScene()
+    {
+        Debug.Log("✨ 開始倒數轉場...");
+        
+        yield return new WaitForSeconds(15f); // 等 10 秒
+
+        // 👉 若你有動畫，這裡可以播放動畫、等待它完成
+        // yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("XXX") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f);
+
+        SceneManager.LoadScene("past"); // ← 請改成你要的場景名稱
+}
